3707fcebef261e2fb2c65b4ac2e4ed3062bb32c3
Daniel ikechukwu <danielikechukwu495@gmail.com>
2023-05-25 18:24:55 +0100

    Array

--- GeeksArray/Program.cs
+++ GeeksArray/Program.cs
@@ -6,7 +6,27 @@ namespace CSharpCode
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, welcome to Geeks code.");
+            int[] myArr = new int[] {1,2,34};
+
+            int[] myArr1 = { 1, 2, 34 };
+
+            var myArr2 = new int[] { 3, 4,4};
+
+            int[] myArr3 = {1,2,3,4,5};
+
+            int[] myArr4;
+
+            myArr4 = new int[8] {2,3,4,4,5,6, 7,4};
+
+            string[] myNames = new string[] { "peter", "jojo", "stephen", "kelly", "Ben", "jane" };
+
+            foreach (string i in myNames)
+            {
+                Console.WriteLine(i);
+            };
+
+            Console.WriteLine(myArr.Length);
+
         }
     }
 }
