77cc8944cc7856ffd3e8f808316ea6d81c644d42
Daniel ikechukwu <danielikechukwu495@gmail.com>
2023-05-25 17:16:40 +0100

    Add project files.

---
+++ GeeksArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCode
+{
+    class GeeksArray
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Geeks code.");
+        }
+    }
+}
